e34deb2c7a11052f72731efb78c00a41c7333611
rhodosaur <lesnick@dancode.net>
2019-12-10 16:38:08 +0100

    move annotations from abstractions

--- src/Mvc/Jerrycurl.Data/Metadata/Annotations/IdAttribute.cs
+++ src/Mvc/Jerrycurl.Data/Metadata/Annotations/IdAttribute.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Jerrycurl.Mvc.Metadata.Annotations
+namespace Jerrycurl.Data.Metadata.Annotations
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
     public class IdAttribute : Attribute
