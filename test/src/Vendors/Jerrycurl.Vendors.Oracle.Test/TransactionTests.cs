84433b23d8758a083f1a47a3dfa0b1d2916fe4e6
rhodosaur <lesnick@dancode.net>
2020-01-23 13:04:06 +0100

    add multi-command tran tests

--- test/src/Vendors/Jerrycurl.Vendors.Oracle.Test/TransactionTests.cs
+++ test/src/Vendors/Jerrycurl.Vendors.Oracle.Test/TransactionTests.cs
@@ -66,5 +66,122 @@ namespace Jerrycurl.Vendors.Oracle.Test
 
             this.helper.VerifyTransaction();
         }
+
+
+        public void Test_MultiInsert_WithoutExplicitTransaction()
+        {
+            this.helper.CreateTable();
+
+            CommandData[] commands = this.GetMultiInsertCommands();
+            CommandHandler handler = this.helper.GetCommandHandler();
+
+            try
+            {
+                handler.Execute(commands);
+            }
+            catch (DbException) { }
+
+            this.helper.VerifyNonTransaction();
+        }
+
+        public async Task Test_MultiInsertAsync_WithoutExplicitTransaction()
+        {
+            this.helper.CreateTable();
+
+            CommandData[] commands = this.GetMultiInsertCommands();
+            CommandHandler handler = this.helper.GetCommandHandler();
+
+            try
+            {
+                await handler.ExecuteAsync(commands);
+            }
+            catch (DbException) { }
+
+            this.helper.VerifyNonTransaction();
+        }
+
+        public void Test_MultiInsert_WithExplicitTransaction()
+        {
+            this.helper.CreateTable();
+
+            CommandData[] commands = this.GetMultiInsertCommands();
+            CommandHandler handler = this.helper.GetCommandHandler(new TransactionFilter());
+
+            try
+            {
+                handler.Execute(commands);
+            }
+            catch (DbException) { }
+
+            this.helper.VerifyTransaction();
+        }
+
+        public async Task Test_MultiInsertAsync_WithExplicitTransaction()
+        {
+            this.helper.CreateTable();
+
+            CommandData[] commands = this.GetMultiInsertCommands();
+            CommandHandler handler = this.helper.GetCommandHandler(new TransactionFilter());
+
+            try
+            {
+                await handler.ExecuteAsync(commands);
+            }
+            catch (DbException) { }
+
+            this.helper.VerifyTransaction();
+        }
+
+        public async Task Test_MultiInsertAsync_WithExplicitTransactionScope()
+        {
+            this.helper.CreateTable();
+
+            CommandData[] commands = this.GetMultiInsertCommands();
+            CommandHandler handler = this.helper.GetCommandHandler(new TransactionScopeFilter());
+
+            try
+            {
+                await handler.ExecuteAsync(commands);
+            }
+            catch (DbException) { }
+
+            this.helper.VerifyTransaction();
+        }
+
+        public void Test_MultiInsert_WithExplicitTransactionScope()
+        {
+            this.helper.CreateTable();
+
+            CommandData[] commands = this.GetMultiInsertCommands();
+            CommandHandler handler = this.helper.GetCommandHandler(new TransactionScopeFilter());
+
+            try
+            {
+                handler.Execute(commands);
+            }
+            catch (DbException) { }
+
+            this.helper.VerifyTransaction();
+        }
+
+        private CommandData[] GetMultiInsertCommands()
+        {
+            return new CommandData[]
+            {
+                new CommandData()
+                {
+                    CommandText = @"BEGIN
+                                        INSERT INTO ""tran_values"" VALUES(1);
+                                        INSERT INTO ""tran_values"" VALUES(2);
+                                    END;",
+                },
+                new CommandData()
+                {
+                    CommandText = @"BEGIN
+                                        INSERT INTO ""tran_values"" VALUES(NULL);
+                                    END;",
+                },
+            };
+        }
     }
 }
