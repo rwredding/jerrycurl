0b7ee0a77af7346ac53ef526906bdb70c1637238
rhodosaur <lesnick@dancode.net>
2020-03-08 21:27:16 -0700

    add equality impl and tests

--- src/Mvc/Jerrycurl.Relations/Many.cs
+++ src/Mvc/Jerrycurl.Relations/Many.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Jerrycurl.Relations
 {
-    public class Many<T> : IEnumerable<T>
+    public class Many<T> : IEnumerable<T>, IEquatable<T>, IEquatable<Many<T>>
     {
         private T value = default;
 
@@ -21,8 +22,7 @@ namespace Jerrycurl.Relations
         public Many() { }
         public Many(T value)
         {
-            this.value = value;
-            this.HasValue = true;
+            this.Value = value;
         }
 
         public void Clear()
@@ -31,12 +31,42 @@ namespace Jerrycurl.Relations
             this.HasValue = false;
         }
 
-        public IEnumerator<T> GetEnumerator()
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
         {
             if (this.HasValue)
                 yield return this.value;
         }
 
-        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<T>)this).GetEnumerator();
+        public override int GetHashCode() => this.HasValue ? (this.Value?.GetHashCode() ?? 0) : 0;
+        public override bool Equals(object obj)
+        {
+            if (obj is T t)
+                return this.Equals(t);
+            else if (obj is Many<T> mt)
+                return this.Equals(mt);
+
+            return false;
+        }
+
+        public bool Equals(Many<T> other)
+        {
+            if (other == null)
+                return false;
+            else if (other.HasValue)
+                return this.Equals(other.value);
+            else if (!this.HasValue)
+                return true;
+
+            return false;
+        }
+
+        public bool Equals(T other)
+        {
+            if (this.HasValue)
+                return object.Equals(this.Value, other);
+
+            return false;
+        }
     }
 }
---
+++ test/src/Mvc/Jerrycurl.Relations.Test/ManyTests.cs
@@ -0,0 +1,41 @@
+﻿using Jerrycurl.Relations.Tests.Models;
+using Jerrycurl.Test;
+using Shouldly;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jerrycurl.Relations.Tests
+{
+    public class ManyTests
+    {
+        public void Test_Many_Equality()
+        {
+            Many<int> empty = new Many<int>();
+            Many<int> zero = new Many<int>(0);
+            Many<int> one = new Many<int>(1);
+
+            Many<int> empty2 = new Many<int>();
+            Many<int> zero2 = new Many<int>(0);
+            Many<int> one2 = new Many<int>(1);
+
+            empty.Equals(empty2).ShouldBeTrue();
+            empty.Equals(zero).ShouldBeFalse();
+            empty.Equals(0).ShouldBeFalse();
+            empty.Equals(one).ShouldBeFalse();
+            empty.Equals(1).ShouldBeFalse();
+
+            zero.Equals(empty).ShouldBeFalse();
+            zero.Equals(zero2).ShouldBeTrue();
+            zero.Equals(0).ShouldBeTrue();
+            zero.Equals(one).ShouldBeFalse();
+            zero.Equals(1).ShouldBeFalse();
+
+            one.Equals(empty).ShouldBeFalse();
+            one.Equals(zero).ShouldBeFalse();
+            one.Equals(0).ShouldBeFalse();
+            one.Equals(one2).ShouldBeTrue();
+            one.Equals(1).ShouldBeTrue();
+        }
+
+    }
+}
