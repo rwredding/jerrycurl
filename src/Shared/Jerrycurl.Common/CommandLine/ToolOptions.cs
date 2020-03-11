1589d1c0e378fcc93dd2e5948ddbb4b575a67884
rhodosaur <lesnick@dancode.net>
2020-03-11 16:20:36 -0700

    move parsing to ToolOption statics, ditch ToolParser

--- src/Shared/Jerrycurl.Common/CommandLine/ToolOptions.cs
+++ src/Shared/Jerrycurl.Common/CommandLine/ToolOptions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Jerrycurl.CommandLine
@@ -60,5 +61,90 @@ namespace Jerrycurl.CommandLine
 
         public IEnumerator<ToolOption> GetEnumerator() => this.options.GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        #region " Parsing "
+        public static ToolOptions Parse(string[] args)
+        {
+            return new ToolOptions(ParseAndYield(args));
+        }
+
+        private static IEnumerable<ToolOption> ParseAndYield(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (IsOption(args[i]))
+                {
+                    ToolOption option = CreateOption(args[i]);
+
+                    if (option != null)
+                    {
+                        option.Values = args.Skip(i + 1).TakeWhile(s => !IsOption(s)).ToArray();
+
+                        i += option.Values.Length;
+                    }
+
+                    yield return option;
+                }
+            }
+        }
+
+        private static bool IsOption(string s) => s.StartsWith("-");
+
+        private static ToolOption CreateOption(string name)
+        {
+            ToolOption option = new ToolOption();
+
+            if (name.StartsWith("--"))
+                option.Name = name.Remove(0, 2);
+            else if (name.StartsWith("-"))
+                option.ShortName = name.Remove(0, 1);
+            else
+                throw new InvalidOperationException($"Invalid option format '{name}'.");
+
+            return option;
+        }
+        #endregion
+
+        #region " Response files (.rsp) "
+        public static IEnumerable<string> GetResponseFiles(string filePath, Func<string, string> pathResolver = null)
+        {
+            if (filePath == null)
+                throw new ArgumentNullException(filePath);
+
+            return GetResponseFiles(new[] { filePath }, pathResolver);
+        }
+
+        public static IEnumerable<string> GetResponseFiles(IEnumerable<string> filePaths, Func<string, string> pathResolver = null)
+        {
+            if (filePaths == null)
+                throw new ArgumentNullException(nameof(filePaths));
+
+            pathResolver = pathResolver ?? (s => s);
+
+            Stack<string> stack = new Stack<string>(filePaths);
+            HashSet<string> fileList = new HashSet<string>();
+
+            while (stack.Count > 0)
+            {
+                string fileName = stack.Pop();
+                string resolvedPath = pathResolver(fileName);
+
+                if (fileName == null || resolvedPath == null)
+                    continue;
+                else if (!fileList.Contains(fileName))
+                {
+                    fileList.Add(fileName);
+                    
+                    if (fileName.StartsWith("@") && File.Exists(resolvedPath))
+                    {
+                        foreach (string subFileName in File.ReadAllLines(resolvedPath))
+                            stack.Push(subFileName);
+                    }
+                    else
+                        yield return fileName;
+                }
+            }
+        }
+        #endregion
     }
 }
